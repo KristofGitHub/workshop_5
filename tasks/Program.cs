6898f445e2e11ef6d02e8987e3b9f719a59f6448
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-24 21:35:43 +0500

    1st commit: first task

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Задайте массив, заполненный 12-ю случайными числами в диапазоне от -9 до 9. 
+// Найдите сумму отрицательных и положительных элементов массива.
+
+Console.WriteLine("Введите размер массива: ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально возможное значение в ячейке: ");
+int min_value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально возможное значение в ячейке: ");
+int max_value = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+    for(int index = 0; index < size; index++){newArray[index] = new Random().Next(minValue, maxValue + 1);}
+    return newArray;
+}
+
+void PrintArray(int[] arrayExample)
+{
+    int size = arrayExample.Length;
+    for(int index = 0; index < size; index++){Console.Write(arrayExample[index] + " ");}
+    Console.WriteLine();
+}
+
+int[] userArray = CreateRandomArray(array_size, min_value, max_value);
+PrintArray(userArray);
+PosNegSum(userArray);
+
+void PosNegSum(int[] array)
+{
+    int sumPos = 0;
+    int sumNeg = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0) sumPos = sumPos + array[i];
+        else sumNeg = sumNeg + array[i];
+    }
+    Console.WriteLine($"Сумма положительных элементов массива равна {sumPos}." + 
+    $" Сумма отрицательных элементов массива равна {sumNeg}.");
+}
